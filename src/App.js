c0eb5593e95af46119a1a490554ee30b5c9150f4
Milgo <carolynemilgo@gmail.com>
2020-06-24 01:38:08 +0300

    learning React-addedoption to add new travel destinations

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ListDestinations from './ListDestinations'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            destinations: [],
+            currentDestination : {
+                text: '',
+                key: ''
+            }
+        }
+        //bind the functions to the context of the App component
+        this.handleInput=this.handleInput.bind(this);
+        this.addDestination=this.addDestination.bind(this);
+    }
+    //handle destinations added in the input field 
+    handleInput(e){
+        this.setState({
+            currentDestination : {
+                text: e.target.value,
+                key: Date.now()
+            }
+        })
+
+    }
+    addDestination(e){
+        //prevent page from refreshing when add destination button is clicked
+        e.preventDefault();
+        const newDestination = this.state.currentDestination;
+        if(newDestination.text !== ""){
+            //create new variable to combine newDestination and grab all the contents of destinations
+            const updatedDestinations=[...this.state.destinations,newDestination];
+            this.setState({
+                destinations : updatedDestinations,
+                currentDestination : {
+                    key : '',
+                    text: '' //reset entire currentDestination fields to empty values
+                }
+
+            })
+        }
+
+
+    }
+
+
+    render(){
+        return(<div>
+            <form onSubmit={this.addDestination}>
+                <input 
+                    value={this.state.currentDestination.text}
+                    placeholder ="Enter destination visited"
+                    onChange = {this.handleInput}
+                    />
+                    
+                    <button type="submit">Add destination</button>
+
+            </form>
+            <p>Destinations Covered</p>
+            <ListDestinations 
+            destinations = {this.state.destinations} />
+        </div>)
+    }
+
 }
+
+
 
 export default App;
---
+++ src/ListDestinations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function ListDestinations(props){
+    return(<div>
+        <p>My Destinations will appear here</p>
+    </div>)
+}
+
+
+export default ListDestinations;
